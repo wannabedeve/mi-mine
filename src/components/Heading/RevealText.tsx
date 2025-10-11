515478a5d74c7acbf4bdffbb22718ccbbc0eaae3
Khaled Ahmed <unknownlordo@gmail.com>
2025-10-11 12:09:48 +0300

    rev

--- src/components/Heading/RevealText.tsx
+++ src/components/Heading/RevealText.tsx
@@ -1,5 +1,5 @@
 import { cn } from "../Work/utils";
-import { motion } from "framer-motion";
+import { motion, Variants } from "framer-motion";
 
 type TRevealText = {
     word: string;
@@ -17,19 +17,34 @@ export default function RevealText({
 }: TRevealText) {
     const letters = word.split("");
 
-    const revealAnim = {
+    // const revealAnim: Variants = {
+    //     hidden: { opacity: 0, y: "80%" },
+    //     show: (delay: number) => ({
+    //         opacity: 1,
+    //         y: 0,
+    //         transition: {
+    //             type: "linear",
+    //             stiffness: 150,
+    //             delay: delay,
+    //             // duration: 0.5,
+    //         },
+    //     }),
+    // };
+
+
+    const revealAnim: Variants = {
         hidden: { opacity: 0, y: "80%" },
         show: (delay: number) => ({
             opacity: 1,
             y: 0,
             transition: {
-                type: "linear",
-                stiffness: 150,
-                delay: delay,
-                // duration: 0.5,
+                ease: "linear",
+                duration: 0.3,
+                delay,
             },
         }),
     };
+
 
     switch (animationType) {
         case "whileInView":
@@ -84,40 +99,4 @@ export default function RevealText({
                 </>
             );
     }
-
-    // if (whileInView) {
-    //   return (
-    //     <>
-    //       {letters.map((letter, index) => (
-    //         <motion.span
-    //           variants={revealAnim}
-    //           initial={"hidden"}
-    //           whileInView={"show"}
-    //           custom={(index + animDelay) / 8}
-    //           key={index}
-    //           className={cn("inline-block ", className)}
-    //         >
-    //           {letter}
-    //         </motion.span>
-    //       ))}
-    //     </>
-    //   );
-    // } else {
-    //   return (
-    //     <>
-    //       {letters.map((letter, index) => (
-    //         <motion.span
-    //           variants={revealAnim}
-    //           initial={"hidden"}
-    //           animate={"show"}
-    //           custom={(index + animDelay) / 8}
-    //           key={index}
-    //           className={cn("inline-block ", className)}
-    //         >
-    //           {letter}
-    //         </motion.span>
-    //       ))}
-    //     </>
-    //   );
-    // }
 }
